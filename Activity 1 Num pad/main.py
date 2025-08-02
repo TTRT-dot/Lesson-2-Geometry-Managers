961273fcd6705a761c7497fc0b15dfe97eecc8f6
TTRT-dot <xdamer6469@gmail.com>
2025-08-02 17:59:28 +0530

    asdasd

---
+++ Activity 1 Num pad/main.py
@@ -0,0 +1,24 @@
+#Import neccesary libraries
+from tkinter import *
+
+#Create a window
+root=Tk()
+root.title("Number Pad")
+root.geometry("250x300")
+
+#Create Frame to organise elements better
+frame = Frame(master=root,height=200,width=360,bg="#d0efff")
+nums = [[9,8,7],[6,5,4],[3,2,1],["#",0,"*"]]
+
+for i in range(4):
+    #Configure rows and clumns to resize video
+    root.columnconfigure(i,weight=1,minsize=75)
+    root.rowconfigure(i,weight=1,minsize=50)
+    for j in range(0,3):
+        frame=Frame(master=root,relief=SUNKEN,borderwidth=1)
+        frame.grid(row=i,column=j)
+
+        label = Label(master=frame,text=nums[i][j],bg="#d0efff")
+        label.pack(padx=3,pady=3)
+
+root.mainloop()
